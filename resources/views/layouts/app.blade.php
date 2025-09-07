6dae78994e1bafc5ceba6ea9cc04deb857ab19d3
Gusma-crypto <agussulistionoa0@gmail.com>
2025-09-08 02:37:36 +0700

    tambahkan force https javascript

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSRF Token -->
+    <!-- <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="csrf-param-name" content="_token"> -->
+    <!-- Force HTTPS -->
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
     <title>SPP Payment Gateway | @yield('title')</title>
     <link rel="shortcut icon" href="{{ asset('assets/static/images/favicon.ico') }}" type="image/x-icon" />
     <link rel="stylesheet" crossorigin href="{{ asset('assets/compiled/css/app.css') }}">
